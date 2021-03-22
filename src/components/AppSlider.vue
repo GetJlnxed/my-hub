6c10a4678e2af9bf154fe0854fa95b25f4a33730
rromanovsky <r.romanovsky@antraks.ru>
2021-03-22 17:50:58 +0300

    Added Slider Vue-component

---
+++ src/components/AppSlider.vue
@@ -0,0 +1,77 @@
+<template lang="pug">
+input.slider(
+  type="range",
+  :min="minValue",
+  :max="maxValue",
+  v-model="currentValue",
+  :style="{ background: background }"
+)
+</template>
+
+<script>
+  export default {
+    name: "AppSlider",
+    data() {
+      return {
+        currentValue: 0,
+      };
+    },
+    created() {
+      if (this.value) {
+        this.currentValue = this.value;
+      }
+    },
+    props: {
+      value: {
+        type: Number,
+        default: 0,
+        required: false,
+      },
+      minValue: {
+        type: Number,
+        default: 0,
+        required: false,
+      },
+      maxValue: {
+        type: Number,
+        default: 100,
+        required: false,
+      },
+    },
+    computed: {
+      background() {
+        let value = ((this.currentValue - this.minValue) / (this.maxValue - this.minValue)) * 100;
+        return "linear-gradient(to right, #979EE8 0%, #979EE8 " + value + "%, #fff " + value + "%, white 100%)";
+      },
+    },
+  };
+</script>
+
+<style lang="stylus" scoped>
+  .slider
+    -webkit-appearance none
+    width 100%
+    height 10px
+    outline none
+    opacity 0.7
+    transition opacity 0.2s
+    border-radius 15px
+
+    &:hover
+      opacity 1
+
+    &::-webkit-slider-thumb
+      -webkit-appearance none
+      appearance none
+      width 20px
+      height 20px
+      background #4A56C2
+      cursor pointer
+      border-radius 15px
+
+    &::-moz-range-thumb
+      width 25px
+      height 25px
+      background #4CAF50
+      cursor pointer
+</style>
--- src/views/Board.vue
+++ src/views/Board.vue
@@ -9,44 +9,54 @@
     <div class="box search">
       <Search />
     </div>
+    <div class="box sliders">
+      <AppSlider :minValue="10" :maxValue="100" :value="25" />
+      <AppSlider :minValue="10" :maxValue="100" :value="50" />
+      <AppSlider :minValue="10" :maxValue="100" :value="100" />
+    </div>
   </div>
 </template>
 
 <script>
-import AppMenu from "@/components/AppMenu";
-import AppCard from "@/components/AppCard";
-import Search from "@/components/Search";
+  import AppMenu from "@/components/AppMenu";
+  import AppCard from "@/components/AppCard";
+  import Search from "@/components/Search";
+  import AppSlider from "@/components/AppSlider";
 
-export default {
-  name: "Board",
-  components: {
-    AppMenu,
-    AppCard,
-    Search,
-  },
-};
+  export default {
+    name: "Board",
+    components: {
+      AppMenu,
+      AppCard,
+      Search,
+      AppSlider,
+    },
+  };
 </script>
 
 <style lang="stylus" scoped>
-.board
-  display grid
-  grid-template-columns repeat(4, 1fr)
-  padding 2rem
+  .board
+    display grid
+    grid-template-columns repeat(4, 1fr)
+    padding 2rem
 
-  & .box
-    margin 2rem
+    & .box
+      margin 2rem
 
-.app-card
-  grid-column-start 1
-  grid-row-start 1
-  grid-row-end 3
+  .app-card
+    grid-column-start 1
+    grid-row-start 1
+    grid-row-end 3
 
-.app-menu
-  grid-row-start 2
-  grid-column-start 2
+  .app-menu
+    grid-row-start 2
+    grid-column-start 2
 
-.search
-  grid-row-start 1
-  grid-column-start 2
-  grid-column-end 5
+  .search
+    grid-row-start 1
+    grid-column-start 2
+    grid-column-end 5
+
+  .sliders .slider
+    margin 1rem 0
 </style>
